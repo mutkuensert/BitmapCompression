d03bac41616d51022fc8f470554eaf05dbdb96f5
M. Utku Ensert <mutkuensert@hotmail.com>
2024-03-11 23:14:25 +0300

    Update version

--- BitmapCompression/build.gradle.kts
+++ BitmapCompression/build.gradle.kts
@@ -45,7 +45,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.mutkuensert"
                 artifactId = "bitmapcompression"
-                version = "2.1.2"
+                version = "2.1.8"
             }
         }
     }
